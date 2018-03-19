072d76d788c1394ca167a362fac82ab0612fae33
kenji <kenji256@gmail.com>
2018-03-19 16:48:55 +0900

    initial commit

---
+++ Source.cpp
@@ -0,0 +1,72 @@
+﻿#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+#include <windows.h>
+
+TCHAR szClassName[] = TEXT("Window");
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	static HWND hButton;
+	switch (msg)
+	{
+	case WM_CREATE:
+		hButton = CreateWindow(TEXT("BUTTON"), TEXT("このボタンを押してもウィンドウをアクティブにしない"), WS_VISIBLE | WS_CHILD, 0, 0, 0, 0, hWnd, (HMENU)IDOK, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		{
+			typedef BOOL(*fnSetChildWindowNoActivate)(HWND);
+			fnSetChildWindowNoActivate SetChildWindowNoActivate = (fnSetChildWindowNoActivate)GetProcAddress(LoadLibrary(TEXT("user32.dll")), (LPCSTR)2005);
+			if (SetChildWindowNoActivate)
+			{
+				SetChildWindowNoActivate(hButton);
+			}
+		}
+		break;
+	case WM_SIZE:
+		MoveWindow(hButton, 10, 10, 512, 32, TRUE);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, msg, wParam, lParam);
+	}
+	return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInst, LPSTR pCmdLine, int nCmdShow)
+{
+	MSG msg;
+	WNDCLASS wndclass = {
+		CS_HREDRAW | CS_VREDRAW,
+		WndProc,
+		0,
+		0,
+		hInstance,
+		0,
+		LoadCursor(0,IDC_ARROW),
+		(HBRUSH)(COLOR_WINDOW + 1),
+		0,
+		szClassName
+	};
+	RegisterClass(&wndclass);
+	HWND hWnd = CreateWindow(
+		szClassName,
+		TEXT("コントロールをクリックしてもウィンドウをアクティブ化しないようにする"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		0,
+		CW_USEDEFAULT,
+		0,
+		0,
+		0,
+		hInstance,
+		0
+	);
+	ShowWindow(hWnd, SW_SHOWDEFAULT);
+	UpdateWindow(hWnd);
+	while (GetMessage(&msg, 0, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
